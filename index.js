55b4fc58d8dec644e50ca49ba2472b09bb5218ef
yatsun <yatsunlam25@gmail.com>
2021-09-21 01:09:58 -0400

    commit tree 20fafcad25eff3c6c1d9a40a0e8b2dd8ca2e17ed

---
+++ index.js
@@ -0,0 +1,10 @@
+// let divide = () => 2000/100;
+let divide = function() {
+    return 2000/100
+}
+let square = x => x * x;
+// Don't need a parenthesis if only one parameter
+// Use paranthesis only IF two or more parameters
+// Max 3 parameters
+
+let add = (x, y) => x + y;
